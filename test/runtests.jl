9703c79fe6486fe2a53b5972efbd756383fecbfc
Michael Reed <18372368+chakravala@users.noreply.github.com>
2023-07-29 13:27:41 -0400

    deprecated unnecessary feature

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,6 +11,6 @@ using Test
 @test indices(Λ(3).v12) == [1,2]
 @test (@basis ℝ^3; v1 ⊆ v12 && v12 ⊆ V)
 !Sys.iswindows() && @test Λ(62).v32a87Ng == -1Λ(62).v2378agN
-@test Λ.V3 == Λ.C3'
+#@test Λ.V3 == Λ.C3'
 @test Λ(ℝ^14) ⊕ Λ(ℝ^14)' == Λ(TensorBundle(14)⊕TensorBundle(14)')
 
